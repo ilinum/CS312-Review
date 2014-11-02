0656ff4d2ccaf52c208b5fa886eb6d2bf0034c90
ilinum <stasi@utexas.edu>
2014-11-02 01:19:47 -0500

    Added a bunch of examples with arrays

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -2,14 +2,62 @@ import java.util.Arrays;
 
 /**
  * Created by Svyatoslav Ilinskiy on 11/1/14.
+ *
+ * Class that contains following examples concerning arrays:
+ *
+ * 1. Accessing elements
+ * 2. DefaultValues
+ * 3. Passing arrays as parameters
+ * 4. Assigning arrays to each other
+ * 5. Testing arrays for equality
+ *
  */
 public class ArrayExamples {
 
     public static void main(String[] args) {
+//        accessElements();
 //        defaultValuesInt();
 //        defaultValuesDouble();
 //        defaultValuesBooleans();
 //        defaultValuesStrings();
+//        arraysAsParametersEasy();
+//        arraysAsParametersHard();
+//        arraysSet();
+//        arrayEquality();
+
+        //examples to do:
+        //1. Write a method that reverses all elements in the array
+        //2. Write a method that switches every two adjacent values in an array
+        //3. Find a mode in an array
+        //4. Merge two sorted arrays
+        //5. Determine how many elements in a String array are set to null
+        //More examples here: http://www.cs.utexas.edu/~scottm/cs312/handouts/slides/topic25_more_array_algorithms.pdf
+    }
+
+    /**
+     * Method with an example how to access elements
+     */
+    public static void accessElements() {
+        int[] array = {10, 2, 4, 1, 3, 6};
+        //we access:
+        // first element: a[0]
+        // last element a[n - 1] where n is the length of the array
+        System.out.println(array[0]); //what will this print?
+        System.out.println(array[4]);
+        System.out.println(array[5]);
+        System.out.println(array[6]);
+        printArray(array);
+        //let's print all of the values!
+        //we could use Arrays.toString(array) but that's too easy
+    }
+
+    /**
+     * prints an array
+     * @param array array to print
+     */
+    public static void printArray(int[] array) {
+        System.out.println(Arrays.toString(array));
+//        System.out.println(array); //fix this method!
     }
 
     /**
@@ -62,5 +110,120 @@ public class ArrayExamples {
         System.out.println("stringArray[2]: " + stringArray[2]);
         //what do all values contain?
         System.out.println(Arrays.toString(stringArray));
+    }
+
+    /**
+     * Method with an example of an array passed as a parameter
+     */
+    public static void arraysAsParametersEasy() {
+        int[] a = {2, 1, 5, -10, -1, 2, 6, 23, 6653};
+        arrayManipulation(a);
+        //what is 'a' now?
+        printArray(a);
+    }
+
+    /**
+     * Do some stuff with array b
+     * @param b array to mess with
+     */
+    public static void arrayManipulation(int[] b) {
+        b[3] = 0;
+        b[1] = -1;
+        //what does b and a have now?
+        printArray(b);
+        b = new int[9];
+        b[3] = 1;
+        b = new int[2];
+        b[1] = 3;
+//        printArray(b);
+    }
+
+    /**
+     * Method with an example of an array passed as a parameter
+     */
+    public static void arraysAsParametersHard() {
+        int[] c = {2, 1, 5, -10, -1, 2, 6, 23, 6653};
+        printArray(c);
+        arrayManipulation(c, c);
+        //what is 'a' now?
+        printArray(c);
+    }
+
+    /**
+     * Do some stuff with 2 arrays
+     * @param c first array
+     * @param d second array
+     */
+    public static void arrayManipulation(int[] c, int[] d) {
+        c[1] = -123;
+        //what is d[1]?
+        System.out.println("d[1]: " + d[1]);
+        d[d.length - 1] = 756;
+        //what is c[c.length - 1]?
+        System.out.println("c[c.length - 1]: " + c[c.length - 1]);
+
+        c[5] = d[4];
+        //so c[5] is equal to d[4] now
+        areEqual(c[5], d[4]);
+        //is d[5] equal to c[4]
+        areEqual(d[5], c[4]);
+        //is d[5] equal to d[4]?
+        areEqual(d[5], c[5]);
+        printArray(d);
+        printArray(c);
+    }
+
+    /**
+     * A simple method that prints equal if two ints are equal and  "Not equal!" if they are not
+     * @param a first argument
+     * @param b second argument
+     */
+    public static void areEqual(int a, int b) {
+        if(a == b) {
+            System.out.println("Equal!");
+        } else {
+            System.out.println("Not equal!");
+        }
+    }
+
+    /**
+     * Method with example concerning what happens when arrays are set to each other
+     */
+    public static void arraysSet() {
+        int[] a = {12, 2, 3, -1, 2};
+        int[] b = a;
+        b[3] = 52;
+        //what is a[3]?
+        System.out.println(a[3]);
+        a[1] = 1257;
+        //what is b[1]?
+        System.out.println(b[1]);
+    }
+
+    /**
+     * Method with an example for array equality
+     */
+    public static void arrayEquality() {
+        int[] a = {10};
+        int[] b = {10};
+        //are they equal?
+        testEquality(a, b);
+         b = a;
+        //are they equal now?
+        testEquality(a, b);
+        b[0] = 2;
+        //are they still equal now?
+        testEquality(a, b);
+    }
+
+    /**
+     * Method that test if two arrays are equal in different ways
+     * @param a first array
+     * @param b second array
+     */
+    public static void testEquality(int[] a, int[] b) {
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a.equals(b): " + (a.equals(b)));
+        System.out.println("Arrays.equals(a, b): " + Arrays.equals(a, b));
     }
 }
